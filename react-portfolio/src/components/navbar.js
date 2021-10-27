6d82127c9969d7df8d16250c3176a719d2e81d04
valbona1992 <valbona12@gmail.com>
2021-10-27 18:12:15 -0500

    update navbar component with links

---
+++ react-portfolio/src/components/navbar.js
@@ -0,0 +1,23 @@
+const NavBar = ({currentPage, handlePageChange}) => {
+    const navLinks = ["About", "Portfolio", "Resume", "Contact"];
+
+    return (
+        <nav> 
+            <ul className= "nav nav-tabs">
+                {navLinks.map((link) => (
+                    <li className="nav-item" key={link}>
+                        <a href= {"#" + link.toLowerCase()}
+                        onClick={() => {
+                            handlePageChange(link);
+                        }}
+                        className={currentPage === link ? "nav-link navActive" : "nav-link"}>
+                        {link}
+                        </a>
+                    </li>
+                 ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
