d3364c8b77cdc7dc19ad95525f59316c4770c472
valbona1992 <valbona12@gmail.com>
2021-10-27 17:57:05 -0500

    updated header component info

---
+++ react-portfolio/src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from "../components/navbar";
+
+const Header = (props) => {
+    return (
+        <header> 
+            <div>
+
+            </div>
+            <NavBar /> 
+        </header>
+    )
+}
+
+export default Header;
