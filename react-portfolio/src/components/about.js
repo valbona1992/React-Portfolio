7740a6935e8e07906738f97fe7226dceb7098bd8
valbona1992 <valbona12@gmail.com>
2021-11-02 15:27:37 -0500

    add info on about me page

--- react-portfolio/src/components/about.js
+++ react-portfolio/src/components/about.js
@@ -2,24 +2,33 @@ import profilePhoto from '../assets/images/ValbonaBajrami.jpg'
 
 const About = () => {
     return (
-        <>
-            <h1>About Me</h1>
-             <div className="about-me"></div> 
-             <div>
-          <p>Hey! I'm Valbona.</p>
-          <p> 
-            text
-          </p>
-          {/* <hr /> */}
-          <p>
-           text
-          </p>
+
+<>
+    <main className="container">
+        <div className="row aboutpage m-5">
+            <div className="col-12 col-md-5 about">
+            <img className=" img-fluid mt-5" alt="headshot of Valbona Bajrami" src={profilePhoto}/> 
+            </div>
+            <div className="col-12 col-md-6 ml-5 ">
+                <h1 className="display-4 text-primary"> About <span className="text-danger fw-bold"> Me </span></h1>
+                <h5 className="text-muted mt-3 "> 
+                         For the past few years, I have been investing my time in pursuing my passions in healthcare as a
+                        naturopathic doctor and acupuncturist. My love for medicine led me to the tech-world as I noticed a
+                        gap with healthcare professionals not being able to provide services and education because of the
+                        lack of web designs for the medical field. I plan to use the knowledge I have learned as a
+                        full-stack developer to help bridge the gap in medicine and tech. Outside of my professional life, my hobbies including hiking, camping, reading, and traveling the world.</h5> 
+                <h5 className="text-muted mt-2"> Take a look at my resume to learn more about the prior experiences I had that has led me here
+                    today.</h5>
+            </div>
         </div>
-        <img src={profilePhoto} alt="headshot of Val" />
-      
+    </main>
+    
+    <footer className="footer mt-auto py-3">
+        <div className="container text-center mt-4">
+          <span className="text-muted">© 2021 Valbona Bajrami</span>
+        </div>
+    </footer>
     </>
-
-
     )
 }
 export default About;
--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-
 import React from 'react';
 import NavBar from "../components/navbar";
+import logo from '../assets/images/ValbonaBajrami.jpg'
 
 
 
@@ -9,15 +9,24 @@ const Header = () => {
     const [currentPage, handlePageChange] = useState("About me");
 
     return (
-        <header> 
-            <div className="header-block">
-            <div className="dev-name">Valbona Bajrami </div>
-             </div>
-            <NavBar 
-                 currentPage={currentPage}
-                 handlePageChange={handlePageChange}
-            /> 
-        </header>
+        <>
+<header>
+<nav className="navbar navbar-expand-lg navbar-light m-4 p-3">
+    <div className="container-fluid ">
+    <img src="images/logo.png" alt="" width="200" height="100" className="d-inline-block align-text-top"/>
+    <button className="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse flex-row-reverse" id="navbarNavAltMarkup">
+        <NavBar
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+            />
+    </div>
+    </div>
+ </nav>
+</header>
+</>
     )
 }
 
