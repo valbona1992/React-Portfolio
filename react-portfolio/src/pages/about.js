c785f99d6df036ba5b95ea42960ba92b89e22e04
valbona1992 <valbona12@gmail.com>
2021-11-02 22:18:39 -0500

    update resume to page

--- react-portfolio/src/pages/about.js
+++ react-portfolio/src/pages/about.js
@@ -1,4 +1,5 @@
 import profilePhoto from '../assets/images/ValbonaBajrami.jpg'
+import resume from "../assets/resume/Valbona_Bajrami_Resume.pdf"
 
 const About = () => {
     return (
@@ -9,7 +10,7 @@ const About = () => {
            
             <div className="col-12 col-md-6 ml-5 ">
                 <h1 className="display-4 text-primary"> Hey! <span className="text-danger fw-bold"> I'm Valbona. </span></h1>
-                <h6 className="text-info"> Full-stack Developer</h6>
+                <h6 className="text-info mt-3"> Full-stack Developer</h6>
                 <h6 className="text-info"> Naturopathic Doctor && Acupuncturist</h6>
                 <h5 className="text-muted mt-3 "> 
                          For the past few years, I have been investing my time in pursuing my passions in healthcare as a
@@ -17,6 +18,8 @@ const About = () => {
                         gap with healthcare professionals not being able to provide services and education because of the
                         lack of web designs for the medical field. I plan to use the knowledge I have learned as a
                         full-stack developer to help bridge the gap in medicine and tech. Outside of my professional life, my hobbies including hiking, camping, reading, and traveling the world.</h5> 
+            <a href="../portfolio" class="btn btn-primary btn-md mt-3">Projects</a>
+            <a href={resume} class="btn btn-primary btn-md mt-3" target="_blank">Resume</a>
             </div>
 
             <div className="col-12 col-md-5 about">
--- react-portfolio/src/pages/resume.js
+++ react-portfolio/src/pages/resume.js
@@ -1,19 +1,22 @@
-import resume from "../assets/resume/Valbona_Bajrami_Resume.pdf";
+import resume from "../assets/resume/Valbona_Bajrami_Resume.pdf"
 
 const Resume = () => {
     return (
         <>
         <div class=" projects">
             <h1 class="display-4 text-primary text-center">  Resume </h1>
-            <h5 class="text-info text-center"> Download my resume 
+            <h5 class="text-info text-center mt-3"> Download my resume 
             <a href={resume} download> here
               <span className="icon icon-download" aria-label="download icon"></span>
             </a></h5>
+            <div class="text-center mt-3">
             <iframe
           src={resume}
           allow="autoplay"
           title="Valbona's Resume"
+          height="500" width="920"
         ></iframe>
+        </div>
         </div>
 
         </>
