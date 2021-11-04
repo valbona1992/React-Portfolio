d34ba3881e687c93894058e7fa2ab006c118d69e
valbona1992 <valbona12@gmail.com>
2021-11-03 21:46:53 -0500

    update small errors in app js

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css' 
 import './index.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +17,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
+
+// I have a mistake somewhere
